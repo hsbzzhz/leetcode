f91df4b70f9f92d9bb1e921daba9933d46f060e7
Hogan Zhang <hsbzzhz@126.com>
2020-12-18 16:12:50 +0800

    12.18

---
+++ recursion/merge_sort.py
@@ -0,0 +1,27 @@
+def mergeSort(arr):
+    import math
+    if (len(arr) < 2):
+        return arr
+    middle = ((len(arr) + 1) // 2)
+    left, right = arr[0:middle], arr[middle:]
+    return merge(mergeSort(left), mergeSort(right))
+
+
+def merge(left: [], right: []):
+    result = []
+    while left and right:
+        if left[0] <= right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+    while left:
+        result.append(left.pop(0))
+    while right:
+        result.append(right.pop(0))
+    return result
+
+
+# test case
+srr = [13, 4, 5, 12, 15, 21, 16, 23, 89]
+res = mergeSort(srr)
+print(res)
---
+++ test/sub-string.py
@@ -0,0 +1,18 @@
+
+def longestPalindrome(s: str) -> str:
+    if len(s)<2 or s[0]==s[-1]:
+        return s
+    reserve = ''
+    for i in range(len(s)):
+        for j in range(i+1, len(s)-1):
+            if s[i] == s[j] and s[i] != s[j+1]:
+                reserve = s[i:j+1] if len(reserve) < len(s[i:j+1]) else reserve
+                return reserve
+            elif s[i] == s[-1]:
+                return s
+    return s[0]
+
+
+sr = 'abb'
+res = longestPalindrome(sr)
+print(res)
