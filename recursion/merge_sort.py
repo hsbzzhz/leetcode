8dbc5bf2830ee204c63e9178bf50de801c520a09
Hogan <hsbzzhz@126.com>
2020-12-29 20:24:46 +0800

    in

--- recursion/merge_sort.py
+++ recursion/merge_sort.py
@@ -1,19 +1,25 @@
 def mergeSort(arr):
-    import math
     if (len(arr) < 2):
         return arr
-    middle = ((len(arr) + 1) // 2)
-    left, right = arr[0:middle], arr[middle:]
+    middle = len(arr) // 2
+    left, right = arr[:middle], arr[middle:]
     return merge(mergeSort(left), mergeSort(right))
 
-
-def merge(left: [], right: []):
+# merge 是治， mergeSort是分
+def merge(left, right):
+    """
+    :param left:
+    :param right:
+    :return: 把左右两个有序list合并成一个, 直至左右list中仅存在一个元素
+    """
     result = []
     while left and right:
+        # 把小的元素弹出
         if left[0] <= right[0]:
             result.append(left.pop(0))
         else:
             result.append(right.pop(0))
+    # 单个list存在元素的情况
     while left:
         result.append(left.pop(0))
     while right:
