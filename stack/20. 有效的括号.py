dbc06b3bc7341d4b838460b1af25d76194ae74e8
Hogan <hsbzzhz@126.com>
2021-02-09 11:44:18 +0800

    20

---
+++ stack/20. 有效的括号.py
@@ -0,0 +1,37 @@
+"""
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+ 
+示例 1：
+输入：s = "()"
+输出：true
+"""
+
+class Solution:
+    """
+    思路：
+    1。把左括号入栈
+    2。如果是右括号，pop出栈顶左括号（所以只入栈了一半的字符串）
+    3。最后如果为空栈，即匹配成功
+    """
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0: return False
+
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        stack = list()
+        for each in s:
+            if each in pairs:
+                if not stack or stack[-1] != pairs[each]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(each)
+        return not stack
