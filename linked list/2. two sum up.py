c028e762cc7dc1cc61cad57fc6bce940b0763584
Hogan <hsbzzhz@126.com>
2021-02-10 16:52:53 +0800

    61

---
+++ linked list/2. two sum up.py
@@ -0,0 +1,26 @@
+"""
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807.
+"""
+
+
+def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def dfs(l, r, i):
+        """
+        :param l: l1
+        :param r: l2
+        :param i: 用来保存进位
+        :return: 合并后的node
+        """
+        if not l and not r and not i: return None
+        s = (l.val if l else 0) + (r.val if r else 0) + i
+        node = ListNode(s % 10)# 当前位的值
+        node.next = dfs(l.next if l else None, r.next if r else None, s // 10)
+        return node
+
+    return dfs(l1, l2, 0)
---
+++ linked list/61. reserve linked list.py
@@ -0,0 +1,30 @@
+"""
+给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+
+输入: 0->1->2->NULL, k = 4
+输出: 2->0->1->NULL
+解释:
+向右旋转 1 步: 2->0->1->NULL
+向右旋转 2 步: 1->2->0->NULL
+向右旋转 3 步: 0->1->2->NULL
+向右旋转 4 步: 2->0->1->NULL
+
+"""
+def rotateRight(self, head: ListNode, k: int) -> ListNode:
+    if not head or k == 0: return head
+    tail, length = head, 1
+    while tail.next:
+        tail, length = tail.next, length + 1
+
+    k, temp = k % length, head
+    if k == 0:
+        return head
+
+    for _ in range(length - k - 1):
+        temp = temp.next
+
+    new_head = temp.next
+    temp.next = None
+    tail.next = head
+
+    return new_head
