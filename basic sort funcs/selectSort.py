050f41834ce088fedc3eb8bad775ef6d66bd89f7
Hogan Zhang <hsbzzhz@126.com>
2021-03-10 17:42:03 +0800

    select sort

--- basic sort funcs/selectSort.py
+++ basic sort funcs/selectSort.py
@@ -3,21 +3,15 @@ class SortDemo(object):
     O(n^2)
     """
     def selectSort(self, nums: list):
-
-        def findSmallest(nums: list):
-            smallest = nums[0]
-            smallest_index = 0
-            for i in range(1, len(nums)):
-                if nums[i] < smallest:
-                    smallest = nums[i]
-                    smallest_index = i
-            return smallest_index
-
-        res = []
-        for i in range(len(nums)):
-            smallest = findSmallest(nums)
-            res.append(nums.pop(smallest))
-        return res
+        for i in range(len(nums) - 1):
+            # 将起始元素设为最小元素
+            min_index = i
+            for j in range(i+1, len(nums)):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+            # 最小元素塞到起始位置
+            nums[min_index], nums[i] = nums[i], nums[min_index]
+        return nums
 
 
 raw = [10, 6, 1, 0, 5]
