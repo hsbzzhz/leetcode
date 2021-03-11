315316b159eab3a63ee1abdeca3436099049a03a
Hogan Zhang <hsbzzhz@126.com>
2021-03-11 17:57:04 +0800

    mergeSort

--- basic sort funcs/mergeSort.py
+++ basic sort funcs/mergeSort.py
@@ -2,10 +2,23 @@ class SortDemo(object):
     """
     O(nlogn)
     """
-    def mergeSort(self, nums:[], l, m, r):
-        pass
+    def mergeSort(self, nums: [int], l, r):
+        def _merge(nums: [int], l, r):
+            #  
+            pass
+
+        if l >=r:
+            return
+        mid = l +r //2
+        # 分开
+        self.mergeSort(nums, l, mid)
+        self.mergeSort(nums, mid, r)
+        # 合并
+        _merge()
 
 
 
 
+res = [2, 9, 3, 6, 4, 1]
 
+print(res)
