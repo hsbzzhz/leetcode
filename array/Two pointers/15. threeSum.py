399c76a1a8733fe2232f59ba58e5c363f59f53e8
Hogan Zhang <hsbzzhz@126.com>
2021-05-06 11:23:32 +0800

    三数之和

--- array/Two pointers/15. threeSum.py
+++ array/Two pointers/15. threeSum.py
@@ -31,20 +31,20 @@ class Solution(object):
                 break  # 特殊情况，最小的数字都大于0
             if i > 0 and nums[i] == nums[i - 1]:
                 continue  # 保证第第一个数 index i 不会重复
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                if nums[i] + nums[l] + nums[r] == 0:
-                    res.append((nums[i], nums[l], nums[r]))
-                    l += 1
-                    r -= 1
-                    # 判断左界和右界是否和下一位置重复，去除重复解。并同时将 L,RL,R 移到下一位置，寻找新的解
-                    while r > l and nums[r] == nums[r + 1]:
-                        r -= 1
-                    while r > l and nums[l] == nums[l - 1]:
-                        l += 1
+            left, right = i + 1, len(nums) - 1  # 左指针只会右移动，右指针只会左移
+            while left < right:
+                if nums[i] + nums[left] + nums[right] == 0:
+                    res.append((nums[i], nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+                    # 判断左界和右界是否和下一位置重复，去除重复解。并同时将 L,R 移到下一位置，寻找新的解
+                    while left < right and nums[right] == nums[right + 1]:   # 跳过重复的, right的上一位是 right + 1
+                        right -= 1
+                    while left < right and nums[left] == nums[left - 1]:   # 跳过重复的， left 的上一位是 left - 1
+                        left += 1
 
-                elif nums[i] + nums[l] + nums[r] > 0:
-                    r -= 1
-                else:
-                    l += 1
+                elif nums[i] + nums[left] + nums[right] > 0:
+                    right -= 1
+                elif nums[i] + nums[left] + nums[right] < 0:
+                    left += 1
         return res
