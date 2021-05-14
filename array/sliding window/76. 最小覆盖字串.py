d337e564d33d605c04e15888d27d90051c05c612
Hogan Zhang <hsbzzhz@126.com>
2021-05-14 18:00:40 +0800

    76.最小覆盖

--- array/sliding window/76. 最小覆盖字串.py
+++ array/sliding window/76. 最小覆盖字串.py
@@ -28,11 +28,44 @@ class Solution:
                 return False
         return True
 
+    def minWindow0(self, s: str, t: str) -> str:
+        # 方法2，自写, 避免使用记录字符的窗口
+        target = {}  # 初始化target字典
+        for each in t:
+            if each in target:
+                target[each] += 1
+            else:
+                target[each] = 1
+        # 设置窗口左右边界
+        left, right = 0, 0
+        res, is_run = s, 0
+        while right < len(s):
+            # 开始遍历整个 原始字符串
+            char_r = s[right]
+            if char_r in target:  # 维护整个target
+                target[char_r] -= 1
+            right += 1
+
+            while self.check(target):  # 如果是正确答案的情况下，就可以开始优化
+                length = right - left
+                if length < len(res):
+                    res = ''.join(s[left: right])   # 如果当前结果比res好，即更新当前答案
+                # 优化开始
+                char_l = s[left]
+                if char_l in target:
+                    target[char_l] += 1
+                left += 1
+                is_run = 1
+
+        if is_run:
+            return res
+        return ''
+
     def minWindow(self, s: str, t: str) -> str:
         # 把 t 种的字符全部放入hashmap中，出现1次就+1
         target = {}
         for each in t:
-            # 如果是 'aa' 的测试用例，则需改动val值
+            # 为了避免同一个字符出现多次，例如 'aa' ，则需-1值, 而不是直接赋值0
             if each in target:
                 target[each] += 1
             else:
