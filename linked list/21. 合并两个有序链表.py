26521c892b939861b6e31b025cbfe11ee935e706
Hogan Zhang <hsbzzhz@126.com>
2021-06-07 13:30:21 +0800

    priority queue 1.0

--- linked list/21. 合并两个有序链表.py
+++ linked list/21. 合并两个有序链表.py
@@ -22,8 +22,8 @@ class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         # 递归终止条件
         if not l1: return l2
-        if not l2: return l1
-        if l1.val < l2.val:
+        elif not l2: return l1
+        elif l1.val < l2.val:
             l1.next = self.mergeTwoLists(l1.next, l2)
             return l1
         else:
