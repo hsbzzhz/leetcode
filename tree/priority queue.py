5a9c90182920739b022fd884573ad12229add703
Hogan <hsbzzhz@126.com>
2021-06-21 00:18:38 +0800

    segement tree

--- tree/priority queue.py
+++ tree/priority queue.py
@@ -1,6 +1,7 @@
 class PriorityQueue:
     """
     大顶堆
+    https://github.com/labuladong/fucking-algorithm/blob/master/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%E7%B3%BB%E5%88%97/%E4%BA%8C%E5%8F%89%E5%A0%86%E8%AF%A6%E8%A7%A3%E5%AE%9E%E7%8E%B0%E4%BC%98%E5%85%88%E7%BA%A7%E9%98%9F%E5%88%97.md
     """
     __queue = []
     # __size = 0
@@ -13,10 +14,17 @@ class PriorityQueue:
         return self.__queue[1]
 
     def insert(self, key):
-        pass
+        self.__N +=1
+        self.__queue.append(key)
+        self.swim(self.__N)
 
     def delete_max_item(self):
-        pass
+        max = self.__queue[1]
+        self.exchange_items(1, self.__N)
+        self.__queue.pop()
+        self.__N-=1
+        self.sink(1)
+        return max
 
     def swim(self, index):
         # 将元素浮至堆顶
@@ -26,8 +34,16 @@ class PriorityQueue:
             index, parent  = parent, index
 
     def sink(self, index):
-        #
-        while index*2 
+        # 如果沉底，就沉不下去了。如果右边节点存在，比一下大小
+        while index*2 <= self.__N:
+            # 假设左边比右边大，
+            left = index*2
+            if left < self.__N and self.__queue[left] < self.__queue[left+1]:
+                left = left+1
+            if self.__queue[left] < self.__queue[index]:
+                break
+            self.exchange_items(index, left)
+            index = left
 
     def exchange_items(self, i, j):
         self.__queue[i], self.__queue[j] = self.__queue[j], self.__queue[i]
---
+++ tree/segment tree.py
@@ -0,0 +1,67 @@
+import List
+class SegmentTree():
+    """
+    线段树
+    https://www.bilibili.com/video/BV1cb411t7AM?t=386
+    """
+    def build_tree(self, arr: list, tree: list, node, start, end):
+        """
+
+        :param arr:
+        :param tree:
+        :param node: 树的根节点
+        :param start: arr的start
+        :param end: arr的end
+        :return:
+        """
+        if start == end:
+            tree[node] = arr[start]  # 递归出口，到了树的底部
+        else:
+            mid = (start + end)//2  # 根据范围求得根节点index
+            left_node = node*2 +1
+            right_node = node*2 +2
+            self.build_tree(arr, tree, left_node, start, mid)
+            self.build_tree(arr, tree, right_node, mid+1, end)
+            tree[node] = tree[left_node] + tree[right_node]
+
+    def update_tree(self, arr: list, tree: list, node, start, end, idx, val):
+        """
+        更改某个节点的值
+        :param arr:
+        :param tree:
+        :param node:
+        :param start: 搜索范围
+        :param end: 搜索范围
+        :param idx:
+        :param val:
+        :return:
+        """
+        if start == end:
+            arr[idx] = val
+            tree[node] = val
+        else:
+            mid = (start + end)//2
+            left_node = node*2 + 1
+            right_node = node*2 + 2
+            if start <= idx<= mid:
+                self.update_tree(arr, tree, left_node, start, mid, idx, val)
+            elif idx > mid:
+                self.update_tree(arr, tree, right_node, mid+1, end, idx, val)
+            tree[node] = tree[left_node] + tree[right_node]
+
+    def query_tree(self, arr: list, tree: list, node, start, end, range_l, range_r):
+        if range_r < start or range_l > end:
+            return 0
+        elif range_l <= start and end <= range_r:
+            return tree[node]
+        elif start == end:
+            return tree[node]
+        else:
+            mid = (start + end) //2
+            left_node = 2*node +1
+            right_node = 2*node +2
+            sum_left = self.query_tree(arr, tree, left_node, start, mid, range_l, range_r)
+            sum_right = self.query_tree(arr, tree, right_node, mid + 1, end, range_l, range_r)
+
+            return sum_left + sum_right
+
