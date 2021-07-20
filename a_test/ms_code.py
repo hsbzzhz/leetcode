cbc10d6309bd940470f14311a89784a0c90a6b7a
Hogan <hsbzzhz@126.com>
2021-07-20 09:55:56 +0800

    这都不出来

--- a_test/ms_code.py
+++ a_test/ms_code.py
@@ -6,3 +6,12 @@ AA01 = 20 + AB00
 求公式内的循环调用
 union find？
 """
+def str_match_0(s, target):
+	for i in range(len(s)):
+		for j in range(len(target)):
+			if target[j] != s[i]:
+				break
+			i +=1
+			if j>=len(target)-1:
+				return i-len(target) + 1
+	return -1
