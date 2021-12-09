7b8b4fa48121808793bee02c7afe310632336ddd
Hogan Zhang <hsbzzhz@126.com>
2021-12-09 22:51:58 +0800

    up

---
+++ 回溯/矩阵中的路径.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class Solution:
+    """
+    剑指 Offer 12. 矩阵中的路径      medium
+    给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
+
+    单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+    输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+    输出：true
+
+    """
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def dfs(i, j, k):
+            """
+            :param i:  二维矩阵 i
+            :param j:  二维矩阵 j
+            :param k:  当前匹配进度，在word上的索引
+            :return:
+            """
+            if not 0 <= i < len(board) or not 0 <= j < len(board[0]) or board[i][j] != word[k]: return False
+            if k == len(word) - 1: return True
+            board[i][j] = ''  # 标记当前矩阵元素，防止重复访问，直接跳过第一个元素
+            # 递归顺序是 下-上-右-左
+            res = dfs(i + 1, j, k + 1) or dfs(i - 1, j, k + 1) or dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1)
+            board[i][j] = word[k]  # 还原数组
+            return res
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(i, j, 0): return True
+        return False
+
+
+# 作者：jyd
+# 链接：https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/solution/mian-shi-ti-12-ju-zhen-zhong-de-lu-jing-shen-du-yo/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
