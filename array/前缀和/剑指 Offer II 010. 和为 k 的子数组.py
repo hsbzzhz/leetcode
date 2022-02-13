5ccdbd81b95360be4fef732b99df4fae0fb597bd
Hogan <hsbzzhz@126.com>
2022-02-13 16:15:52 +0800

    剑指 Offer II 010. 和为 k 的子数组.py plus

--- array/前缀和/剑指 Offer II 010. 和为 k 的子数组.py
+++ array/前缀和/剑指 Offer II 010. 和为 k 的子数组.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     """
     给定一个整数数组和一个整数k ，请找到该数组中和为k的连续子数组的个数。
@@ -38,4 +41,48 @@ class Solution:
             else:  # 如果此时的前缀和没有出现过，则初始化为 1
                 pre_sum_map[pre_sum] = 1
 
+        return count
+
+class Solution1:
+    """
+    剑指 Offer II 011. 0 和 1 个数相同的子数组
+    给定一个二进制数组 nums , 找到含有相同数量的 0 和 1 的最长连续子数组，并返回该子数组的长度。
+
+
+
+    示例 1:
+
+    输入: nums = [0,1]
+    输出: 2
+    说明: [0, 1] 是具有相同数量 0 和 1 的最长连续子数组。
+    示例 2:
+
+    输入: nums = [0,1,0]
+    输出: 2
+    说明: [0, 1] (或 [1, 0]) 是具有相同数量 0 和 1 的最长连续子数组。
+    """
+    def findMaxLength(self, nums: List[int]) -> int:
+        """
+        前缀数，但是这题并不用生成数组
+        只要需要知道最新的和，对比hashmap里的相同元素的序号，即可求得最长距离
+
+        :param nums:
+        :return:
+        """
+        index_dict = {0: -1}
+        count = 0
+        pre_sum_index = 0
+        for index, num in enumerate(nums):
+
+            if num == 1:
+                pre_sum_index += 1
+            else:
+                pre_sum_index -= 1
+
+            if pre_sum_index in index_dict:
+                count = max(count, index - index_dict[pre_sum_index])
+            else:
+                index_dict[pre_sum_index] = index
+
+            # print(count)
         return count
