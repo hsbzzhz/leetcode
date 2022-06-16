04f13537409f49e1d82c39d17f7cb528557bfea0
Hogan <hsbzzhz@126.com>
2022-06-16 08:11:11 +0800

    update

--- array/Two pointers/26. 删除排序数组中的重复项.py
+++ array/Two pointers/26. 删除排序数组中的重复项.py
@@ -4,14 +4,14 @@
 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
 
 
-示例 1:
+示例1:
 
 给定数组 nums = [1,1,2],
 
 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
 
 你不需要考虑数组中超出新长度后面的元素。
-示例 2:
+示例2:
 
 给定 nums = [0,0,1,1,1,2,2,3,3,4],
 
@@ -32,11 +32,10 @@ def removeDuplicates(self, nums):
     """
     if not nums:
         return 0
-    slow = 0
-    for fast in range(len(nums)):
-        # 前后相同，就继续遍历直到快慢指针指向不同的数
+    fast, slow = 0, 0
+    while fast < len(nums):
         if nums[fast] != nums[slow]:
-            # 如果前后不同，有效数字加一并且把快指针的有效数值赋值给慢指针的下一位，这样就越过了中间的重复项
-            slow += 1  # 这个步骤要在赋值之前
+            slow += 1
             nums[slow] = nums[fast]
+        fast += 1
     return slow + 1
