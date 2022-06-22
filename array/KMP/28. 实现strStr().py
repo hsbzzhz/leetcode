5a1c4b7b86fb09c42ca913580a69ceaf03ed9b79
Hogan <hsbzzhz@126.com>
2022-06-22 23:31:22 +0800

    @Hogan todo, implement KMP

--- array/KMP/28. 实现strStr().py
+++ array/KMP/28. 实现strStr().py
@@ -1,14 +1,14 @@
 class Solution:
     """
-    实现 strStr() 函数。
+    实现strStr()函数。
     7.20 日 微软电话面试题
-    给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1 。
+    给你两个字符串haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回 -1 。
 
     说明：
 
-    当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+    当needle是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
 
-    对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
+    对于本题而言，当needle是空字符串时我们应当返回 0 。这与 C 语言的strstr()以及 Java 的indexOf()定义相符。
 
 
     示例 1：
@@ -24,7 +24,7 @@ class Solution:
     链接：https://leetcode-cn.com/problems/implement-strstr
     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
     """
-
+    #  这答案有问题，@todo improve
     def strStr(self, haystack: str, needle: str) -> int:
         if len(needle) == 0:
             return 0
